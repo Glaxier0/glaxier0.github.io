e5d9ca8c295e603343097c9ba61e0107ef725580
glaxier0 <tahir2001@hotmail.com>
2022-04-04 22:10:41 +0300

    Final form of code

--- script.js
+++
@@ -1,9 +0,0 @@
-function onClick() {
-  var randomColor = Math.floor(Math.random()*16777215).toString(16);
-  const background = document.querySelector("body > div > div.one");
-  background.style.backgroundColor = "#" + randomColor;
-  console.log('hi');
-}
-
-const button = document.querySelector('button');
-button.addEventListener('click', onClick);
